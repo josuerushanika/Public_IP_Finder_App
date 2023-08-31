6ae49ffa15a3395f513208323c9cad6257e62b91
josuerushanika <josuerushanika@gmail.com>
2023-09-01 01:39:37 +0200

    Update fetch API

--- ip_finder/src/Components/IpAddress.jsx
+++ ip_finder/src/Components/IpAddress.jsx
@@ -1,10 +1,25 @@
+import { useEffect, useState } from "react";
 
 const IpAddress = () => {
-    (
+    const [IpAddressDict, setIpAddressDict] = useState({});
+    useEffect(() =>{
+        fetch("http://ip-api.com/json/").then((res) => {
+            if (res.ok) {
+                return res.json()
+            } else {
+                throw Error("Failed to fetch IP Addess")
+            }
+        }).then((data) => {
+            console.log(data)
+        }).catch((error) =>
+        console.log(error))
+    }, []);
+    
+     return (
         <div>
             <h1>'0.0.0.0'</h1>
         </div>
-    );
+    )
 };
 
 export default IpAddress;
