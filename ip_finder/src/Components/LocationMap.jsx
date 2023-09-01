8ec8d621aaee4a9832f4f259f390b28d63281372
josuerushanika <josuerushanika@gmail.com>
2023-09-01 14:05:37 +0200

    Update files

--- ip_finder/src/Components/LocationMap.jsx
+++ ip_finder/src/Components/LocationMap.jsx
@@ -1,5 +1,13 @@
+import Card from "../UI/Card";
+
 const LocationMap = (props) => {
-    return <h1>Image</h1>
+    return <Card>
+        <img src={`https://maps.googleapis.com/maps/api/staticmap?center=${props.lat},%20$
+        {props.lon}&zoom=6&size=400x400&markers=color:red%7C${props.lat},%20${props.lon}&key=AIzaSyAWZ9QUI_bRFnTghPg0yAOhkkpaGwXt3k
+        `} alt="" />
+    </Card>
 };
 
 export default LocationMap;
+
+//${GCP_MAP_API_KEY}
